57a74ca07a967942ff9ab278d493db39efec7cfd
manthanadesai <47625105+manthanadesai@users.noreply.github.com>
2019-02-20 11:51:04 +0530

    Add files via upload

---
+++ ocr.py
@@ -0,0 +1,47 @@
+
+
+# USAGE
+# python ocr.py --image images/example_01.png 
+# python ocr.py --image images/example_02.png  --preprocess blur
+
+# import the necessary packages
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+ap.add_argument("-p", "--preprocess", type=str, default="thresh",help="type of preprocessing to be done")
+args = vars(ap.parse_args())
+
+# load the example image and convert it to grayscale
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Image", gray)
+
+
+if args["preprocess"] == "thresh":
+	gray = cv2.threshold(gray, 0, 255,
+		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+elif args["preprocess"] == "blur":
+	gray = cv2.medianBlur(gray, 3)
+
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, gray)
+
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print(text)
+
+
+
+cv2.imshow("Output", gray)
+cv2.waitKey(0)
